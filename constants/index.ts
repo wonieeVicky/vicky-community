58fa735d60c077bb51654f5c64a930dc102b455f
wonieeVicky <hwfongfing@gmail.com>
2025-09-02 01:11:38 +0900

    feat: InputField 컴포넌트 생성 및 GRAY 컬러 상수 추가

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,13 @@
 ﻿const colors = {
   WHITE: "#ffffff",
   BLACK: "#000000",
-  ORANGE_600: "#FF6B57"
+  ORANGE_600: "#FF6B57",
+  GRAY_100: "#F6F6F6",
+  GRAY_200: "#E2E8F0",
+  GRAY_300: "#D1D5DB",
+  GRAY_500: "#6B7280",
+  GRAY_600: "#4B5563",
+  GRAY_700: "#374151"
 };
 
 export { colors };
