905ad49dcd00eabda608c12070d6c73e830e7192
wonieeVicky <hwfongfing@gmail.com>
2025-09-04 00:54:22 +0900

    feat: 피드 아이템 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,30 @@
-import CustomButtons from "@/components/CustomButtons";
-import { router } from "expo-router";
-import { Text } from "react-native";
+import FeedItem from "@/components/FeedItem";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Text>Home Screen</Text>
-      <CustomButtons label="버튼" onPress={() => router.push("/auth")} />
+      <FeedItem
+        post={{
+          id: 1,
+          userId: 1,
+          title: "더미 제목입니다.",
+          description:
+            "더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.",
+          createdAt: "",
+          author: {
+            id: 1,
+            nickname: "닉네임",
+            imageUri: ""
+          },
+          imageUris: [],
+          likes: [],
+          hasVote: false,
+          voteCount: 1,
+          commentCount: 1,
+          viewCount: 1
+        }}
+      />
     </SafeAreaView>
   );
 }
---
+++ components/FeedItem.tsx
@@ -0,0 +1,92 @@
+﻿import { colors } from "@/constants";
+import { Post } from "@/types";
+import { Ionicons, MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+interface FeedItemProps {
+  post: Post;
+}
+
+function FeedItem({ post }: FeedItemProps) {
+  const isLiked = true;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{post.title}</Text>
+        <Text numberOfLines={3} style={styles.description}>
+          {post.description}
+        </Text>
+      </View>
+      <View style={styles.menuContainer}>
+        <Pressable style={styles.menu}>
+          <Octicons
+            name={isLiked ? "heart-fill" : "heart"}
+            size={16}
+            color={isLiked ? colors.ORANGE_600 : colors.BLACK}
+          />
+          <Text style={isLiked ? styles.activeMenuText : styles.menuText}>
+            1
+          </Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <MaterialCommunityIcons
+            name="comment-processing-outline"
+            size={16}
+            color={colors.BLACK}
+          />
+          <Text>1</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <Ionicons name="eye-outline" size={16} color={colors.BLACK} />
+          <Text>1</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.WHITE
+  },
+  contentContainer: {
+    padding: 16
+  },
+  menuContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderTopColor: colors.GRAY_300,
+    borderTopWidth: StyleSheet.hairlineWidth
+  },
+  title: {
+    fontSize: 18,
+    color: colors.BLACK,
+    fontWeight: "600",
+    marginVertical: 8
+  },
+  description: {
+    fontSize: 16,
+    color: colors.BLACK,
+    marginBottom: 14
+  },
+  menu: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 16,
+    width: "33.33%",
+    gap: 4
+  },
+  menuText: {
+    fontSize: 14,
+    color: colors.GRAY_700
+  },
+  activeMenuText: {
+    fontWeight: "500",
+    color: colors.ORANGE_600
+  }
+});
+
+export default FeedItem;
