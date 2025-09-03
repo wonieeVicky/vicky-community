94be1f827d060b5c83ad08586e120e66713f9664
wonieeVicky <hwfongfing@gmail.com>
2025-09-04 01:03:14 +0900

    feat: FlatList를 활용한 FeedList 컴포넌트 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,19 @@
-import FeedItem from "@/components/FeedItem";
+import FeedList from "@/components/FeedList";
+import { colors } from "@/constants";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <FeedItem
-        post={{
-          id: 1,
-          userId: 1,
-          title: "더미 제목입니다.",
-          description:
-            "더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.",
-          createdAt: "",
-          author: {
-            id: 1,
-            nickname: "닉네임",
-            imageUri: ""
-          },
-          imageUris: [],
-          likes: [],
-          hasVote: false,
-          voteCount: 1,
-          commentCount: 1,
-          viewCount: 1
-        }}
-      />
+    <SafeAreaView style={styles.container}>
+      <FeedList />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.WHITE
+  }
+});
---
+++ components/FeedList.tsx
@@ -0,0 +1,122 @@
+﻿import { colors } from "@/constants";
+import { FlatList, StyleSheet } from "react-native";
+import FeedItem from "./FeedItem";
+
+const dummyData = [
+  {
+    id: 1,
+    userId: 1,
+    title: "더미 제목입니다.",
+    description:
+      "더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "닉네임",
+      imageUri: ""
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1
+  },
+  {
+    id: 2,
+    userId: 2,
+    title: "더미 제목입니다.",
+    description:
+      "더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "닉네임",
+      imageUri: ""
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1
+  },
+  {
+    id: 3,
+    userId: 3,
+    title: "더미 제목입니다.",
+    description:
+      "더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "닉네임",
+      imageUri: ""
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1
+  },
+  {
+    id: 4,
+    userId: 4,
+    title: "더미 제목입니다.",
+    description:
+      "더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "닉네임",
+      imageUri: ""
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1
+  },
+  {
+    id: 5,
+    userId: 5,
+    title: "더미 제목입니다.",
+    description:
+      "더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.",
+    createdAt: "",
+    author: {
+      id: 1,
+      nickname: "닉네임",
+      imageUri: ""
+    },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1
+  }
+];
+
+function FeedList() {
+  return (
+    <FlatList
+      data={dummyData}
+      renderItem={({ item }) => <FeedItem post={item} />}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={styles.contentContainer}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 12,
+    backgroundColor: colors.GRAY_200,
+    gap: 12
+  }
+});
+
+export default FeedList;
