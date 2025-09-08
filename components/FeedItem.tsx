81d1849bb8bb1c54fc6b7a8ea448b440d786b6e6
wonieeVicky <hwfongfing@gmail.com>
2025-09-09 00:22:14 +0900

    feat: Profile component 구현

--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -2,6 +2,7 @@
 import { Post } from "@/types";
 import { Ionicons, MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
+import Profile from "./Profile";
 
 interface FeedItemProps {
   post: Post;
@@ -13,6 +14,12 @@ function FeedItem({ post }: FeedItemProps) {
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
+        <Profile
+          imageUri={post.author.imageUri}
+          nickname={post.author.nickname}
+          createdAt={post.createdAt}
+          onPress={() => {}}
+        />
         <Text style={styles.title}>{post.title}</Text>
         <Text numberOfLines={3} style={styles.description}>
           {post.description}
---
+++ components/Profile.tsx
@@ -0,0 +1,69 @@
+﻿import { colors } from "@/constants";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+
+interface ProfileProps {
+  onPress: () => void;
+  imageUri?: string;
+  nickname: string;
+  createdAt: string;
+  option?: React.ReactNode;
+}
+
+function Profile({
+  onPress,
+  imageUri,
+  nickname,
+  createdAt,
+  option
+}: ProfileProps) {
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.profileContainer} onPress={onPress}>
+        <Image
+          source={
+            imageUri
+              ? { uri: imageUri }
+              : require("@/assets/images/default-avatar.png")
+          }
+          style={styles.avatar}
+        />
+        <View style={{ gap: 4 }}>
+          <Text style={styles.nickname}>{nickname}</Text>
+          <Text style={styles.createdAt}>{createdAt}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.GRAY_300
+  },
+  nickname: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: colors.BLACK
+  },
+  createdAt: {
+    fontSize: 14,
+    color: colors.GRAY_500
+  }
+});
+
+export default Profile;
