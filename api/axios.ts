df8b7d687749e4299bfe2f78037f834f1175517c
wonieeVicky <hwfongfing@gmail.com>
2025-09-14 22:40:13 +0900

    feat: SecureStore를 통한 로그인 API 구현

---
+++ api/axios.ts
@@ -0,0 +1,13 @@
+﻿import axios from "axios";
+import { Platform } from "react-native";
+
+const BASE_URL = {
+  android: "http://10.0.2.2:3030",
+  ios: "http://localhost:3030"
+};
+
+const axiosInstance = axios.create({
+  baseURL: Platform.OS === "ios" ? BASE_URL.ios : BASE_URL.android
+});
+
+export default axiosInstance;
---
+++ utils/secureStore.ts
@@ -0,0 +1,32 @@
+﻿import * as SecureStore from "expo-secure-store";
+
+/**
+ * 키와 값을 저장
+ * @param key
+ * @param value
+ */
+async function saveSecureStore(key: string, value: string) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+/**
+ * 키를 이용해 값을 가져옴
+ * @param key
+ * @returns
+ */
+
+async function getSecureStore(key: string) {
+  const storedData = (await SecureStore.getItemAsync(key)) ?? null;
+  return storedData;
+}
+
+/**
+ * 키를 이용해 값을 삭제
+ * @param key
+ * @returns
+ */
+async function deleteSecureStore(key: string) {
+  await SecureStore.deleteItemAsync(key);
+}
+
+export { deleteSecureStore, getSecureStore, saveSecureStore };
